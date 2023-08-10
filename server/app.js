0c212ef41584326538d3996881730d25d8ba5f2d
ahmed-alanasweh <ahmed.anaswah@optimumpartners.com>
2023-08-10 18:26:15 +0300

    graphql

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { createHandler } = require("graphql-http/lib/use/express");
+const expressPlayground =
+  require("graphql-playground-middleware-express").default;
+const mongoose = require("mongoose");
+
+const app = express();
+const schema = require("./graphql/schema/index");
+const resolvers = require("./graphql/resolvers/index");
+app.use(bodyParser.json());
+
+app.all(
+  "/graphql",
+  createHandler({
+    schema,
+    rootValue: resolvers,
+  })
+);
+
+app.get("/playground", expressPlayground({ endpoint: "/graphql" }));
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.eqhsz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(4000, () => {
+      console.log("now listening for requests on port 4000");
+    });
+  })
+  .catch((err) => console.log(err));
